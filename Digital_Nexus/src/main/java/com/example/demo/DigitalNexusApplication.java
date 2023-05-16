5a805ce69d8fece28c5890eac64a3dafde96cf2f
JhanSw <jhanrcc@hotmail.com>
2023-05-16 09:02:07 -0500

    Commit desde el entorno

---
+++ Digital_Nexus/src/main/java/com/example/demo/DigitalNexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalNexusApplication.class, args);
+	}
+
+}
