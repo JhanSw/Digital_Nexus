d6bdd735d910ef775fa116f8fc15dbe39c6dbef9
JhanSw <jhanrcc@hotmail.com>
2023-05-18 13:59:44 -0500

    CRUD Finalizado

--- Digital_Nexus/src/main/java/com/example/demo/service/UserService.java
+++ Digital_Nexus/src/main/java/com/example/demo/service/UserService.java
@@ -5,4 +5,12 @@ import com.example.demo.entity.User;
 public interface UserService {
 
 	public Iterable<User> getAllUsers();
+
+	public User createUser(User user) throws Exception;
+	
+	public User getUserById(Long id) throws Exception;
+	
+	public User updateUser(User user) throws Exception;
+	
+	public void deleteUser(Long id) throws Exception;
 }
